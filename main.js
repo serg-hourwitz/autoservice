1604475626deaa835e06ae5b1011db9ceef291d7
Serhiy Hourwitz <shmone800@gmail.com>
2024-12-09 19:01:35 +0200

    added about us responsive styles & scripts

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Burger-nav */
+
 const burgerBtn = document.querySelector('.header-nav-burger');
 const navList = document.querySelector('.header-nav-list');
 
@@ -16,4 +18,29 @@ const navItems = document.querySelectorAll('.header-nav-item');
 navItems.forEach(item => {
   item.addEventListener('click', navClose)
 });
+
+/* hidden text */
+
+document.addEventListener('DOMContentLoaded', () => {
+  const readMoreLink = document.querySelector('.read-more');
+  const readLessLink = document.querySelector('.read-less');
+  const hiddenText = document.querySelector('.text-hidden-hidden');
+
+  const toggleVisibility = (isVisible) => {
+    hiddenText.classList.toggle('visible', isVisible); // Текст стає видимим/прихованим
+    readMoreLink.classList.toggle('hidden', isVisible); // Ховаємо/показуємо "читати далі"
+  };
+
+  readMoreLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleVisibility(true); // Розгортаємо текст
+  });
+
+  readLessLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleVisibility(false); // Згортаємо текст
+  });
+});
+
+
 
